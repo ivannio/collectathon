a7f8c8490caaaf5062732a750a832f683e6acd6f
Ivan Phelps <ivanniothemannio@gmail.com>
2020-01-23 18:45:22 -0600

    cards complete save for minor bug causing flip to delay

--- src/components/pages/SearchGameCard/SearchGameCard.js
+++ src/components/pages/SearchGameCard/SearchGameCard.js
@@ -6,7 +6,7 @@ class SearchGameCard extends React.Component {
   render() {
     const { game } = this.props;
     const { genres } = this.props.game;
-    const printGenres = () => genres.map((g) => g.name).join(', ');
+    const { platforms } = this.props.game;
 
     return (
       <div className="flip-card">
@@ -18,7 +18,15 @@ class SearchGameCard extends React.Component {
         </div>
         <div className="search-game-card-back">
           <h1 className="game-header-back">{game.name}</h1>
-          <p className="genres">{printGenres()}</p>
+          <p className="genres">{genres.map((g) => g.name).join(', ')}</p>
+          <div className="additional-info">
+            <p className="released">Released: {game.released}</p>
+            <p className="platforms">Platforms: {platforms.map((p) => p.platform.name).join(', ')}</p>
+            <p className="metacritic">Metacritic Rating:  {isNaN(game.metacritic) ? 'No score' : game.metacritic}</p>
+          </div>
+          <div className="add-to-collection">
+            <p className="add-to-collection-text">add to collection</p><span className="plus-icon"><i className="far fa-plus-square fa-4x"></i></span>
+          </div>
         </div>
         </div>
       </div>
