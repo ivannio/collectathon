de0636854bb65e2d9991f415c2b7ba87c053d0d9
Ivan Phelps <ivanniothemannio@gmail.com>
2020-02-05 18:12:27 -0600

    hours are correct now

--- src/components/pages/SteamGameCard/SteamGameCard.js
+++ src/components/pages/SteamGameCard/SteamGameCard.js
@@ -3,11 +3,23 @@ import React from 'react';
 import './SteamGameCard.scss';
 
 class SteamGameCard extends React.Component {
+  formatPlaytime = (playtime) => {
+    let hourString = (playtime / 60).toString();
+    const decimalPosition = hourString.indexOf('.');
+    const splicePosition = decimalPosition + 2;
+    if (decimalPosition !== -1) {
+      hourString = hourString.slice(0, splicePosition);
+    } else {
+      console.log('');
+    }
+    return hourString;
+  }
+
   render() {
     const { steamGame } = this.props;
     return (
       <div className="steam-game-card">
-       <img className="steam-game-img" alt="steam-game-logo" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${steamGame.appid}/${steamGame.img_logo_url}.jpg`}></img><div className="steam-deets"><p className="steam-game-title">{steamGame.name}</p><div className="total-playtime">Total Playtime: {steamGame.playtime_forever} hrs</div></div>
+       <img className="steam-game-img" alt="steam-game-logo" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${steamGame.appid}/${steamGame.img_logo_url}.jpg`}></img><div className="steam-deets"><p className="steam-game-title">{steamGame.name}</p><div className="total-playtime">Total Playtime: {this.formatPlaytime(steamGame.playtime_forever)} hrs</div></div>
       </div>
     );
   }
